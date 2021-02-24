126b2e8a2dde23e3d55e68a31a13127f8f55f73d
unknown <wish.lynx@gmail.com>
2021-02-24 14:16:29 -0500

    solves discrete log

---
+++ shanks.py
@@ -0,0 +1,36 @@
+import math
+## set vars ##
+g = 2
+h = 893
+modulus = 1373
+g_powers = {}
+h_inverses = {}
+
+n = 1 + math.floor(math.sqrt(modulus-1))
+print(n)
+
+for i in range(n):
+    g_powers[i] = pow(g, i, modulus)
+
+print(g_powers)
+print()
+
+
+## g^(-1*n), and assuming g and modulus are relatively prime
+inverse = pow(g, (modulus-2)*n, modulus)
+
+for j in range(n): 
+    h_inverses[j] =  (h*pow(inverse, j)) % modulus
+    
+
+print(h_inverses)
+print()
+
+for keyq in g_powers:
+    for keyp in h_inverses:
+        if g_powers[keyq] == h_inverses[keyp]:
+            x = keyq+(keyp*n)
+            print('discrete log: ', x)
+            break
+            
+
